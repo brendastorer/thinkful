ddddc71a52ddcbe728367df95d01ea2e1aded79b
Brenda Storer <brendastorer@gmail.com>
2017-08-19 20:57:31 -0400

    Show styled results from a YouTube search

--- thinkful-tube/scripts.js
+++ thinkful-tube/scripts.js
@@ -3,25 +3,41 @@ const YOUTUBE_API_KEY = "AIzaSyCDJlkF9sKY73Fl6C5UvNFikSVLv5-2ZN8";
 
 function getDataFromApi(searchTerm, callback) {
   const query = {
-    maxResults: 10,
+    maxResults: 12,
     part: "snippet",
-    q: "`${searchTerm}`",
+    q: searchTerm,
     key: YOUTUBE_API_KEY
   }
   const result = $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
 function renderResult(result) {
+  const thumbnail = result.snippet.thumbnails.medium.url;
+  const title = result.snippet.title;
+  const id = result.id.videoId;
+  const url = `https://www.youtube.com/watch?v=${id}`;
+  
   return `
-    <h2>${result}</h2>
+    <li class="search-results__item">
+      <a class="search-results__link" href="${url}" target="_blank">
+        <img class="search-results__thumbnail" src="${thumbnail}" />
+        <h2 class="search-results__title">${title}</h2>
+      </a>
+    </li>
   `;
 }
 
 function displayYouTubeSearchData(data) {
+  const resultsContainer = $(".js-search-results");
+  
+  resultsContainer.empty();
   data.items.forEach(function(item) {
-    const title = item.snippet.title;
-    console.log(title);
+    resultsContainer.append(renderResult(item));
   });
+}
+
+function clearInput(input) {
+  input.val("");
 }
 
 function watchSubmit() {
@@ -29,7 +45,8 @@ function watchSubmit() {
     event.preventDefault();
     const queryTarget = $(event.currentTarget).find(".js-search-input");
     const query = queryTarget.val();
-    queryTarget.val("");
+
+    clearInput(queryTarget);
     getDataFromApi(query, displayYouTubeSearchData);
   });
 }
